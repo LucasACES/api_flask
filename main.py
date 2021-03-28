2ed8b4774565501a449d4dca25dbbe624aefeb00
Lucas Cesar <lucasc.a01@outlook.com>
2021-03-28 19:31:18 -0300

    update api

--- main.py
+++ main.py
@@ -1,39 +1,5 @@
-import json
-import os
-
-from psycopg2 import connect
-from psycopg2.extras import Json
-from flask import Flask, jsonify, request
-
-app = Flask(__name__)
+from src.server.instance import server
+from src.controllers.dados import *
 
 
-jira_json_file = open(file='jira_formatted.json')
-trello_json_file = open(file='trello_formatted.json')
-
-# Lendo o arquivo como Json.
-jira_data = json.load(jira_json_file)
-trello_data = json.load(trello_json_file)
-
-# Fechando o arquivo, visto que ele não é mais necessário.
-jira_json_file.close()
-trello_json_file.close()
-
-
-@app.route('/jira/')
-def jira():
-    if request.headers.get('a') == '42':
-        return jsonify(jira_data), 200
-    return jsonify(jira_data), 200
-
-
-@app.route('/trello/')
-def trello():
-    if request.headers.get('a') == '42':
-        return jsonify(trello_data), 200
-    return jsonify(trello_data), 200
-
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+server.run()
---
+++ src/server/instance.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_restplus import Api
+
+
+class Server():
+    def __init__(self,):
+        self.app = Flask(__name__)
+        self.api = Api(self.app,
+                       version='1.0',
+                       title='ProjetoGSW',
+                       description='dados Trello e Jira',
+                       doc='/doc',
+                       )
+
+    def run(self,):
+        self.app.run(
+            debug=True,
+        )
+
+
+server = Server()
