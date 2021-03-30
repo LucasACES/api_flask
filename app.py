d5b0b2e0a34e3f4020645e4c7e1b759b1ef23728
Lucas Cesar <lucasc.a01@outlook.com>
2021-03-30 09:30:30 -0300

    update

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import json
-
+import os
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -33,5 +33,6 @@ def trello():
         return jsonify(trello_data)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
