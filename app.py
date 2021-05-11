a9294c07c7fd519693f18e5e43c4b99165547912
Lucas Cesar <lucasc.a01@outlook.com>
2021-05-11 09:49:37 -0300

    resolving null of costacomercio

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
+from flask_cors import CORS, cross_origin
 from flask import Flask, jsonify, request
 from controller.function import *
 import os
 import sys
 sys.path.append('/apiGSW/model/funcoes')
-from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 cors = CORS(app, resources={r"/data/*": {"origins": "*"}})
 
+
 @app.route("/data/<opcao>/", methods=["GET"])
 @app.route("/data/<opcao>/<nome>/", methods=["GET"])
 @cross_origin(origin='*', headers=['Content- Type', 'Authorization'])
-def statusbyprojeto(opcao, nome = ''):
+def statusbyprojeto(opcao, nome=''):
     if request.method == "GET":
         projeto = projet(opcao, nome)
     return projeto
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
