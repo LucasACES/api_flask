43094fd20ae14ab0070eb4435024e8a5b4b65f7b
Lucas Cesar <lucasc.a01@outlook.com>
2021-05-12 09:08:02 -0300

    add index

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask_cors import CORS, cross_origin
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from controller.function import *
 import os
 import sys
 sys.path.append('/apiGSW/model/funcoes')
+sys.path.append('/apiGSW/view/')
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='view/templates')
 app.config['CORS_HEADERS'] = 'Content-Type'
 cors = CORS(app, resources={r"/data/*": {"origins": "*"}})
 
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route("/data/<opcao>/", methods=["GET"])
 @app.route("/data/<opcao>/<nome>/", methods=["GET"])
